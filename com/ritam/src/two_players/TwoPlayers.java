26dd4bc8ffc1f8d58ba62971f8ad96719ebc205b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2019-06-05 12:19:52 +0530

    Player now choose their own warrior in both twoPlayers and vsCom

--- com/ritam/src/two_players/TwoPlayers.java
+++ com/ritam/src/two_players/TwoPlayers.java
@@ -1,7 +1,35 @@
 package _Play_Ground.Tic_Tac_Toe.com.ritam.src.two_players;
 
+import java.util.Scanner;
+
 public class TwoPlayers {
+    private Scanner scanner;
+
+    { scanner = new Scanner(System.in); }
+
     public TwoPlayers() {
-        System.out.println("Welcome to 2 Players game mode");
+        System.out.println("Welcome to 2 players game mode");
+
+        while (true) {
+            String choice;
+
+            System.out.println("Choose warrior for Player 1 (O/X): ");
+            choice = scanner.next();
+
+            switch (choice) {
+                case "X":
+                case "x":
+                    System.out.println("Player 2 will play with 'O'");
+                    break;
+                case "O":
+                case "o":
+                    System.out.println("Player 2 will play with X");
+                    break;
+                case "0":
+                    System.exit(2);
+                default:
+                    System.out.println("UNKNOWN INPUT!");
+            }
+        }
     }
 }
--- com/ritam/src/vs_com/VsCom.java
+++ com/ritam/src/vs_com/VsCom.java
@@ -1,7 +1,35 @@
 package _Play_Ground.Tic_Tac_Toe.com.ritam.src.vs_com;
 
+import java.util.Scanner;
+
 public class VsCom {
+    private Scanner scanner;
+
+    { scanner = new Scanner(System.in); }
+
     public VsCom() {
         System.out.println("Welcome to Vs Computer game mode");
+
+        while (true) {
+            String choice;
+
+            System.out.println("Choose your warrior (O/X): ");
+            choice = scanner.next();
+
+            switch (choice) {
+                case "X":
+                case "x":
+                    System.out.println("Computer will play with 'O'");
+                    break;
+                case "O":
+                case "o":
+                    System.out.println("Computer will play with 'X'");
+                    break;
+                case "0":
+                    System.exit(3);
+                default:
+                    System.out.println("UNKNOWN INPUT!");
+            }
+        }
     }
 }
